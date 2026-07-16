1b37072beca99c578580154c22bcee09ad2436c1
Rupaka Studio <rupaka@studio.dev>
2026-07-16 19:29:16 +0700

    Fix admin login routing issue by adding wildcard to route path

--- src/AppRouter.tsx
+++ src/AppRouter.tsx
@@ -24,7 +24,7 @@ export default function AppRouter() {
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<App />} />
-        <Route path="/admin/login" element={<AdminApp />} />
+        <Route path="/admin/login/*" element={<AdminApp />} />
         <Route
           path="/admin/*"
           element={
